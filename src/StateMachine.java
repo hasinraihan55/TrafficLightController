87a90d26131cdb5a913e42b551271c2ff5ec5cc9
hasinraihan55 <hasinr60@gmail.com>
2022-05-09 12:45:39 -0600

    initial 6 states now fully work

--- src/StateMachine.java
+++ src/StateMachine.java
@@ -4,14 +4,22 @@ public class StateMachine {
     public int count = 0; // starts off at 0 initially
 
     // the light states to be set in the machine
-    public int Red = 0; // the red light
-    public int Amber = 0; // the amber light
-    public int Green = 0; // the green light
+    // 0 for red, 1 for green, 2 for amber
+    public int North_Bound = 0; // the lights for north bound
+    public int South_Bound = 0; // the lights for south bound
+    public int East_Bound = 0; // the lights for east bound
+    public int West_Bound = 0; // the lights for west bound
 
     // the walk states to be set in the machine
-    public int walk = 0; // the walk state
-    public int dont_walk = 0; // the don't walk state
-    public int flashing_dont_walk = 0; // the flashing don't walk state
+    // 0 for walk, 1 for don't walk. 2 for flashing don't walk
+    public int North_Bound_Walk = 0; // the lights for north bound
+    public int South_Bound_Walk = 0; // the lights for south bound
+    public int East_Bound_Walk = 0; // the lights for east bound
+    public int West_Bound_Walk = 0; // the lights for west bound
+
+    // left turn signal
+    public int left_turn = 0;
+
 
     // the instantiate of all the possible states of the state machine
     State_1 state1 = new State_1();
@@ -23,47 +31,72 @@ public class StateMachine {
 
     // the main model of the state machine
     public void controller() {
+        state1.setStateTrue(); // initially stay at state 1
         while (true) {
             count++;
             try {
                 Thread.sleep(10);
                 // to stay in state 1
-                if (count >= 0 && count < 10) {
-                    state1.setStateTrue();
+                if (state1.enable && count >= 0 && count <= 60) {
                     state1.setStateTrue();
                 }
-                // to enter and stay in state 2
-                else if (state1.enable && count >= 10 && count < 20) {
+                // reset the counter for state 1
+                else if (state1.enable && count > 60){
+                    count = 0;
                     state1.setStateFalse();
                     state2.setStateTrue();
                 }
-                // to enter and stay in state 3
-                else if (state2.enable && count >= 20 && count < 30) {
+                // to stay in state 2
+                else if (state2.enable && count >= 0 && count <= 6) {
+                    state2.setStateTrue();
+                }
+                // reset the counter for state 2
+                else if (state2.enable && count > 6){
+                    count = 0;
                     state2.setStateFalse();
                     state3.setStateTrue();
                 }
-                // to  enter and stay in state 4
-                else if (state3.enable && count >= 30 && count < 40) {
+                // to stay in state 3
+                else if (state3.enable && count >= 0 && count <= 2) {
+                    state3.setStateTrue();
+                }
+                // reset the counter for state 3
+                else if (state3.enable && count > 2){
+                    count = 0;
                     state3.setStateFalse();
                     state4.setStateTrue();
                 }
-                // to  enter and stay in state 5
-                else if (state4.enable && count >= 40 && count < 50) {
+                // to stay in state 4
+                else if (state4.enable && count >= 0 && count <= 60) {
+                    state4.setStateTrue();
+                }
+                // reset the counter for state 4
+                else if (state4.enable && count > 60){
+                    count = 0;
                     state4.setStateFalse();
                     state5.setStateTrue();
                 }
-                // to  enter and stay in state 6
-                else if (state5.enable && count >= 50 && count < 60) {
+                // to  enter and stay in state 5
+                else if (state5.enable && count >= 0 && count <= 6) {
+                    state4.setStateFalse();
+                    state5.setStateTrue();
+                }
+                // reset the counter for state 5
+                else if (state5.enable && count > 6){
+                    count = 0;
                     state5.setStateFalse();
                     state6.setStateTrue();
                 }
-                else if (count > 60){
-                    state6.setStateFalse();
-                    state1.setStateTrue();
-                    count = 0;
+                // to stay in state 6
+                else if (state6.enable && count >= 0 && count <= 2) {
+                    state6.setStateTrue();
                 }
 
-
+                else if (state6.enable && count > 2){
+                    count = 0;
+                    state6.setStateFalse();
+                    state1.setStateTrue();
+                }
 
                 System.out.println(count);
                 System.out.println("State 1: " + state1.enable);
