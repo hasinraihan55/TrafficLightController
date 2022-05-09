9a3d1d112ebcb2246986b3f27cf7fdeea8e58132
hasinraihan55 <hasinr60@gmail.com>
2022-05-09 14:36:22 -0600

    Added a test driver

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,24 @@
+// test driver for the state machine
 public class Test extends StateMachine {
-    public static void main(String[] args){
-        int count = 0;
+    public static void main(String[] args) {
         StateMachine machine = new StateMachine();
+        machine.state1.setStateTrue();
         while (true) {
-            count++;
-            machine.controller();
-            System.out.println(machine.North_Bound);
+            try {
+                Thread.sleep(10);
+                machine.controller();
+                System.out.println(machine.count);
+                System.out.println("State 1: " + machine.state1.enable);
+                System.out.println("State 2: " + machine.state2.enable);
+                System.out.println("State 3: " + machine.state3.enable);
+                System.out.println("State 4: " + machine.state4.enable);
+                System.out.println("State 5: " + machine.state5.enable);
+                System.out.println("State 6: " + machine.state6.enable);
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
         }
-
     }
 
 }
