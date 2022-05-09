6612c5104fc06c32fb7a250d4bc6555a72445d9f
hasinraihan55 <hasinr60@gmail.com>
2022-05-09 14:39:13 -0600

    Added a test driver

--- src/StateMachine.java
+++ src/StateMachine.java
@@ -31,94 +31,96 @@ public class StateMachine {
 
     // the main model of the state machine
     public void controller() {
-        state1.setStateTrue(); // initially stay at state 1
-        while (true) {
-            count++;
-            try {
-                Thread.sleep(10);
-                // to stay in state 1
-                if (state1.enable && count >= 0 && count <= 60) {
-                    state1.setStateTrue();
-                }
-                // reset the counter for state 1
-                else if (state1.enable && count > 60){
-                    count = 0;
-                    state1.setStateFalse();
-                    state2.setStateTrue();
-                }
-                // to stay in state 2
-                else if (state2.enable && count >= 0 && count <= 6) {
-                    state2.setStateTrue();
-                }
-                // reset the counter for state 2
-                else if (state2.enable && count > 6){
-                    count = 0;
-                    state2.setStateFalse();
-                    state3.setStateTrue();
-                }
-                // to stay in state 3
-                else if (state3.enable && count >= 0 && count <= 2) {
-                    state3.setStateTrue();
-                }
-                // reset the counter for state 3
-                else if (state3.enable && count > 2){
-                    count = 0;
-                    state3.setStateFalse();
-                    state4.setStateTrue();
-                }
-                // to stay in state 4
-                else if (state4.enable && count >= 0 && count <= 60) {
-                    state4.setStateTrue();
-                }
-                // reset the counter for state 4
-                else if (state4.enable && count > 60){
-                    count = 0;
-                    state4.setStateFalse();
-                    state5.setStateTrue();
-                }
-                // to  enter and stay in state 5
-                else if (state5.enable && count >= 0 && count <= 6) {
-                    state4.setStateFalse();
-                    state5.setStateTrue();
-                }
-                // reset the counter for state 5
-                else if (state5.enable && count > 6){
-                    count = 0;
-                    state5.setStateFalse();
-                    state6.setStateTrue();
-                }
-                // to stay in state 6
-                else if (state6.enable && count >= 0 && count <= 2) {
-                    state6.setStateTrue();
-                }
-
-                else if (state6.enable && count > 2){
-                    count = 0;
-                    state6.setStateFalse();
-                    state1.setStateTrue();
-                }
-
-                System.out.println(count);
-                System.out.println("State 1: " + state1.enable);
-                System.out.println("State 2: " + state2.enable);
-                System.out.println("State 3: " + state3.enable);
-                System.out.println("State 4: " + state4.enable);
-                System.out.println("State 5: " + state5.enable);
-                System.out.println("State 6: " + state6.enable);
-            }
-            catch (InterruptedException exception){
-                exception.printStackTrace();
-            }
+        count++;
+        // to stay in state 1
+        if (state1.enable && count >= 0 && count <= 60) {
+            state1.setStateTrue();
+            // set the directional bound main lights
+            North_Bound = 0;
+            South_Bound = 0;
+            East_Bound = 1;
+            West_Bound = 1;
+        }
+        // reset the counter for state 1
+        else if (state1.enable && count > 60){
+            state1.setStateFalse();
+            state2.setStateTrue();
+            count = 0;
+        }
+        // to stay in state 2
+        else if (state2.enable && count >= 0 && count <= 6) {
+            state2.setStateTrue();
+            // set the directional bound main lights
+            North_Bound = 0;
+            South_Bound = 0;
+            East_Bound = 2;
+            West_Bound = 2;
+        }
+        // reset the counter for state 2
+        else if (state2.enable && count > 6){
+            count = 0;
+            state2.setStateFalse();
+            state3.setStateTrue();
+        }
+        // to stay in state 3
+        else if (state3.enable && count >= 0 && count <= 2) {
+            state3.setStateTrue();
+            // set the directional bound main lights
+            North_Bound = 0;
+            South_Bound = 0;
+            East_Bound = 0;
+            West_Bound = 0;
+        }
+        // reset the counter for state 3
+        else if (state3.enable && count > 2){
+            count = 0;
+            state3.setStateFalse();
+            state4.setStateTrue();
+        }
+        // to stay in state 4
+        else if (state4.enable && count >= 0 && count <= 60) {
+            state4.setStateTrue();
+            // set the directional bound main lights
+            North_Bound = 1;
+            South_Bound = 1;
+            East_Bound = 0;
+            West_Bound = 0;
+        }
+        // reset the counter for state 4
+        else if (state4.enable && count > 60){
+            count = 0;
+            state4.setStateFalse();
+            state5.setStateTrue();
+        }
+        // to stay in state 5
+        else if (state5.enable && count >= 0 && count <= 6) {
+            state5.setStateTrue();
+            // set the directional bound main lights
+            North_Bound = 2;
+            South_Bound = 2;
+            East_Bound = 0;
+            West_Bound = 0;
+        }
+        // reset the counter for state 5
+        else if (state5.enable && count > 6){
+            count = 0;
+            state5.setStateFalse();
+            state6.setStateTrue();
+        }
+        // to stay in state 6
+        else if (state6.enable && count >= 0 && count <= 2) {
+            state6.setStateTrue();
+            // set the directional bound main lights
+            North_Bound = 0;
+            South_Bound = 0;
+            East_Bound = 0;
+            West_Bound = 0;
+        }
+        // to reset the entire state machine
+        else if (state6.enable && count > 2){
+            count = 0;
+            state6.setStateFalse();
+            state1.setStateTrue();
         }
     }
-
-
-    public static void main(String[] args){
-
-        StateMachine machine = new StateMachine();
-        machine.controller();
-
-    }
-
-
 }
